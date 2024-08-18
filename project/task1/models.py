33dd90a8237628b92f446b6024c066061a026644
Maxim Ragalevich <ragalevich@yandex.ru>
2024-08-19 01:52:24 +0300

    homework 19.4

--- project/task1/models.py
+++ project/task1/models.py
@@ -1,4 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
 
 
 class Buyer(models.Model):
@@ -14,4 +23,3 @@ class Game(models.Model):
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name="games")
-
